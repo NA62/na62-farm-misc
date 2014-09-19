e744759b87d5b38429cd3983dc603a93239f82f5
Jonas Kunze <kunze.jonas@gmail.com>
2014-09-19 12:40:44 +0200

    implemented setIpmiUsers.sh

---
+++ kickstart/setIPMIUsers.sh
@@ -0,0 +1,16 @@
+read -p "First PC num: " first; echo
+read -p "Last PC num: " last; echo
+read -p "User: " user; echo
+stty -echo
+read -p "Password: " password; echo
+read -p "New Password: " newPassword; echo
+stty echo
+
+for i in `seq $first $last`
+do
+	ipmitool -H na62farm$i-ipmi -U $user -P $password user set name 3 root
+	ipmitool -H na62farm$i-ipmi -U $user -P $password user set password 2 $newPassword
+	ipmitool -H na62farm$i-ipmi -U $user -P $newPassword user set password 3 $newPassword
+	ipmitool -H na62farm$i-ipmi -U $user -P $newPassword user enable 3
+	ipmitool -H na62farm$i-ipmi -U $user -P $newPassword user priv 3 4
+done
