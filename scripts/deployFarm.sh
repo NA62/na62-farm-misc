4cc64053b50511ff14c1761820b0d9b9d9b52458
Jonas Kunze <kunze.jonas@gmail.com>
2014-09-22 19:15:24 +0200

    renamed switchPowerCluster to siwthPower

--- scripts/deployFarm.sh
+++ scripts/deployFarm.sh
@@ -4,10 +4,8 @@ read -p "Debug/Release: " config; echo
 for i in `seq 1 30`
 do
 	echo "installing farm$i"
-#	ssh root@na62farm$i "cp /workspace/na62-farm/$config/na62-farm /usr/local/bin/na62-farm-$version &&
-#	rm /usr/local/bin/na62-farm &&
-#	ln -s /usr/local/bin/na62-farm-$version /usr/local/bin/na62-farm" &
-
-	ssh root@na62farm$i "ln -s /usr/local/bin/na62-farm-$version /usr/local/bin/na62-farm"
+	ssh root@na62farm$i "cp /workspace/na62-farm/$config/na62-farm /usr/local/bin/na62-farm-$version &&
+	rm /usr/local/bin/na62-farm &&
+	ln -s /usr/local/bin/na62-farm-$version /usr/local/bin/na62-farm" &
 done
 
